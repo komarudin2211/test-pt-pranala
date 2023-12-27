8e433b5df77ac89d2a839e54963dc8616220c429
komarudin2211 <komarudin2211@gmail.com>
2023-12-27 12:36:00 +0700

    test-interview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState ,useRef, useEffect} from 'react';
 
 function App() {
+  const [dataInput, setDataInput] = useState(0);
+  const [bilGanjil, setBilGanjil] = useState([]);
+  const [bilPrima, setBilPrima] = useState([]);
+
+
+  const input = (e) => {
+    setDataInput(parseInt(e.target.value));
+  }
+
+  const GenerateGanjil = (angka) => {
+    var arr = [];
+    for(let i=0; i <= angka; i++){
+      if(i % 2 > 0){
+        arr.push(i);
+      }
+    }
+    console.log("arr => ", arr)
+    setBilGanjil(arr);
+    setBilPrima([]);
+  }
+
+  const GeneratePrima = (angka) => {
+    var arr = [];
+    for(let i=0; i <= angka; i++){
+      if(i == 1 || i == 3 || i == 5){
+        arr.push(i);
+      }
+
+      if(i > 5){
+        for(let j=2; j < 10; j++){
+          if(i !=j && i % j > 0){
+            console.log("prima => ", i);
+          }
+        }
+      }
+     
+      // if(i % 2 > 0 &&  i % 3 > 0  && i % 4 > 0 && i % 5 > 0   && i % 6 > 0 && i % 7 > 0  && i % 8 > 0   && i % 9 > 0){
+      //   console.log(i)
+      // }
+    }
+
+    setBilPrima(arr);
+    setBilGanjil([]);
+    
+
+    console.log(bilGanjil);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div><input type="number" onInput={input} value={dataInput} /> </div>
+        <div>{bilGanjil.toString()}</div>
+        <div><button type="button">Generate Segitiga</button></div>
+        <div><button type="button" onClick={() => GenerateGanjil(dataInput)}>Generate Bil. Ganjil</button></div>
+        <div><button type="button" onClick={() => GeneratePrima(dataInput)}>Generate Bil. Prima</button></div>
+      </form>
     </div>
   );
 }
